5362cd447075af9a178530afbf6c4249495efded
aritra <aritrarick2002@gmail.com>
2024-06-09 00:49:34 +0530

    Impl. darkmode in the `Theme`

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,12 +1,17 @@
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import di.appModule
 import navigation.Navigation
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.KoinApplication
+import org.koin.compose.koinInject
 import ui.CoinifyTheme
+import utils.ThemeViewModel
 
 @Composable
 @Preview
@@ -15,7 +20,12 @@ fun App() {
     KoinApplication(application = {
         modules(appModule)
     }) {
-        CoinifyTheme {
+        val themeViewModel: ThemeViewModel = koinInject()
+        val isDarkModeEnabled by themeViewModel.currentTheme.collectAsState(isSystemInDarkTheme())
+
+        CoinifyTheme(
+            darkTheme = isDarkModeEnabled
+        ) {
             MainContent()
         }
     }
