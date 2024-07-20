437f1708c5755d8180ed55f693341f76c92032f1
aritra <aritrarick2002@gmail.com>
2024-07-20 16:16:58 +0530

    Impl. the `Market Trends` in Statistics Screen

--- composeApp/src/commonMain/kotlin/presentation/StatisticsScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/StatisticsScreen.kt
@@ -1,13 +1,18 @@
 package presentation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -20,13 +25,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coinify.composeapp.generated.resources.Res
+import coinify.composeapp.generated.resources.market_trends
 import coinify.composeapp.generated.resources.poppins_medium
+import coinify.composeapp.generated.resources.poppins_regular
 import coinify.composeapp.generated.resources.top_losing
 import coinify.composeapp.generated.resources.top_movers
 import component.StatsCard
@@ -37,6 +45,7 @@ import navigation.Screens
 import org.jetbrains.compose.resources.Font
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.koinInject
+import kotlin.math.roundToInt
 
 @Composable
 fun StatisticsScreen(
@@ -74,6 +83,82 @@ fun StatisticsScreen(
             horizontalAlignment = Alignment.Start,
             verticalArrangement = Arrangement.SpaceBetween
         ) {
+
+            Text(
+                text = stringResource(Res.string.market_trends),
+                style = TextStyle(
+                    color = MaterialTheme.colorScheme.onSurface,
+                    fontSize = 24.sp,
+                    fontFamily = FontFamily(Font(Res.font.poppins_medium))
+                ),
+                textAlign = TextAlign.Start,
+            )
+
+            Spacer(modifier = Modifier.height(15.dp))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .background(MaterialTheme.colorScheme.surface, RoundedCornerShape(16.dp)),
+                contentAlignment = Alignment.Center
+            ) {
+
+                val randomCurrency = data?.data?.random()
+                randomCurrency?.let { data ->
+                    val textColor =
+                        if (data.quote.USD.percentChange24h > 0) Color.Green else Color.Red
+
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Top
+                    ) {
+                        Text(
+                            text = "${data.name} (${data.symbol})",
+                            style = MaterialTheme.typography.titleLarge.copy(
+                                fontFamily = FontFamily(Font(Res.font.poppins_medium))
+                            )
+                        )
+
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                        Text(
+                            text = "$ " + "${((data.quote.USD.price?.times(100))?.roundToInt())?.div(100.0)}",
+                            style = MaterialTheme.typography.titleMedium.copy(
+                                fontFamily = FontFamily(Font(Res.font.poppins_regular))
+                            )
+                        )
+
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(
+                                text = if (data.quote.USD.percentChange24h > 0) "▲" else "▼",
+                                style = MaterialTheme.typography.bodyMedium.copy(
+                                    fontFamily = FontFamily(Font(Res.font.poppins_regular))
+                                )
+                            )
+                            Spacer(modifier = Modifier.width(4.dp))
+
+                            Text(
+                                text = "${data.quote.USD.percentChange24h.roundToInt()}% in 24h",
+                                style = MaterialTheme.typography.bodyMedium.copy(
+                                    fontFamily = FontFamily(Font(Res.font.poppins_regular))
+                                ),
+                                color = textColor
+                            )
+                        }
+                    }
+                }
+            }
+
+            // TOP MOVERS
+
             Text(
                 text = stringResource(Res.string.top_movers),
                 style = TextStyle(
@@ -105,6 +190,8 @@ fun StatisticsScreen(
             }
 
             Spacer(modifier = Modifier.height(20.dp))
+
+            // TOP LOSING
 
             Text(
                 text = stringResource(Res.string.top_losing),
