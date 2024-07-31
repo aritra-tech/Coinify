a6934fd5840af50533b17ac0020eaf54e3c6da6a
aritra <aritrarick2002@gmail.com>
2024-07-31 19:39:46 +0530

    Created Repository

---
+++ composeApp/src/commonMain/kotlin/data/repository/CoinifyRepository.kt
@@ -0,0 +1,30 @@
+package data.repository
+
+import data.database.CoinifyDao
+import domain.model.Data
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.flow.flowOn
+
+class CoinifyRepository(
+    private val dao: CoinifyDao
+) {
+
+    suspend fun upsert(data: Data) {
+        dao.upsert(data)
+    }
+
+    suspend fun delete(data: Data) {
+        dao.delete(data)
+    }
+
+    suspend fun deleteAllBookmark() {
+        dao.deleteAllBookmark()
+    }
+
+    fun getCoins() = dao.getCoins().flowOn(Dispatchers.IO)
+
+    suspend fun getCoin(coinId: Int): Data? {
+        return dao.getCoin(id = coinId)
+    }
+}
