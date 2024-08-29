54fc20b018dbbabdae6f86abbab858449f2c3b49
aritra <aritrarick2002@gmail.com>
2024-08-29 20:51:51 +0530

    Fixed the manifest issue for wear os

--- wearapp/src/main/java/com/aritra/wearapp/presentation/WearApp.kt
+++ wearapp/src/main/java/com/aritra/wearapp/presentation/WearApp.kt
@@ -1,11 +1,17 @@
 package com.aritra.wearapp.presentation
 
 import android.app.Application
+import di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class WearApp: Application() {
     override fun onCreate() {
         super.onCreate()
 
-
+        startKoin {
+            androidContext(this@WearApp)
+            modules(appModule)
+        }
     }
 }
